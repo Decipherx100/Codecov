2b8f26a70891371f4d60871eea3e4faf37e65099
Decipherx100 <70934271+Decipherx100@users.noreply.github.com>
2020-11-30 04:33:50 -0800

    Add test coverage for index.uncovered

--- src/test_index.py
+++ src/test_index.py
@@ -3,11 +3,11 @@ import index
 
 def test_uncovered_if():
     assert index.uncovered_if() == False
-    assert index.uncovered_if()
 
 def test_fully_covered():
     assert index.fully_covered() == True
 
-
+def test_uncovered():
+    assert index.uncovered() == True
 
 
