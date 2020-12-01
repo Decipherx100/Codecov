5634037b004264388f15a541a2585fb92cabf2e3
Decipherx100 <70934271+Decipherx100@users.noreply.github.com>
2020-12-01 03:39:37 -0800

    Update test_demo.py
    Adding back Untested_function

--- src/test_demo.py
+++ src/test_demo.py
@@ -5,5 +5,5 @@ import demo
 def test_tested_function():
     assert demo.tested_function()
 
-#def test_untested_function():
-#    assert demo.untested_function()
+def test_untested_function():
+    assert demo.untested_function()
